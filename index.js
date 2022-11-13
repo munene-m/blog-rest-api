36d4bca8a842b1a77d003e2f2cdd4b64377d0383
munene <munenenjue18@gmail.com>
2022-11-14 00:10:37 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const authRoute = require('./routes/auth')
+
+dotenv.config()
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(console.log("connected to MONGO"))
+.catch(err => console.log(err))
+
+app.use("/auth", authRoute)
+
+app.listen(5000, () => {
+    console.log("Backend running")
+})
